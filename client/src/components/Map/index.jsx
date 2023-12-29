6ac88e1a7a5bd724ac88059919288fc9141c7323
404pandas <113853550+404pandas@users.noreply.github.com>
2023-12-29 16:12:56 -0500

    Map can be drawn on, points created. Bug- All points share a state

--- client/src/components/Map/index.jsx
+++ client/src/components/Map/index.jsx
@@ -1,134 +1,221 @@
-import { useEffect, useRef, useState } from "react";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
-import VectorSource from "ol/source/Vector";
-import VectorLayer from "ol/layer/Vector";
-import Draw from "ol/interaction/Draw";
-import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
-import { LineString, Polygon } from "ol/geom";
-import Feature from "ol/Feature"; // Import Feature
+import { useEffect, useState, useRef } from "react";
+import Draw from "ol/interaction/Draw.js";
+import { Modify } from "ol/interaction.js";
+import Map from "ol/Map.js";
+import View from "ol/View.js";
+import Point from "ol/geom/Point.js";
+import Feature from "ol/Feature.js";
+import { Style, Text } from "ol/style.js";
+import { OSM, Vector as VectorSource } from "ol/source.js";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
+import "./style.css";
 
 const MapComponent = () => {
-  const mapRef = useRef(null);
-  const vectorSource = useRef(new VectorSource());
-  const vectorLayer = useRef(
-    new VectorLayer({
-      source: vectorSource.current,
-    })
-  );
+  // State variables
+  const [map, setMap] = useState(null);
+  const [type, setType] = useState("Point");
+  const [drawInteraction, setDrawInteraction] = useState(null);
+  const [userFeatures, setUserFeatures] = useState([]);
+  const [textValue, setTextValue] = useState("Insert Name");
+  const mapContainerRef = useRef(null);
 
-  const [drawActive, setDrawActive] = useState(false);
-  const [drawType, setDrawType] = useState("Point");
-  const [drawFeature, setDrawFeature] = useState(null);
+  // Handles changes in Type (Point, LineString, Polygon, Circle)
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+  };
 
+  // Adds a user feature to the map
+  const addUserFeature = (geometry) => {
+    setUserFeatures((prevUserFeatures) => [
+      ...prevUserFeatures,
+      new Feature({
+        geometry: geometry,
+      }),
+    ]);
+  };
+
+  // Handles changes in TextValue
+  const handleTextChange = (event) => {
+    setTextValue(event.target.value);
+  };
+
+  // Handles the submission of the text input value
+  const handleSubmit = () => {
+    // Tests textValue call
+    console.log("Submitted Text Value: ", textValue);
+
+    // Updates the style of map features with the submitted textValue
+    if (map) {
+      const vectorLayer = map.getLayers().item(1);
+      if (vectorLayer) {
+        const style = new Style({
+          text: new Text({
+            anchor: [0.5, 46],
+            anchorXUnits: "fraction",
+            anchorYUnits: "pixels",
+            // sets text to state textValue
+            text: textValue,
+          }),
+        });
+
+        // Updates the style of the map features, this might be why all of them are being updated?
+        vectorLayer.setStyle(function () {
+          style.getText().setText(textValue);
+          return style;
+        });
+      }
+    }
+  };
+
+  // Inits map
   useEffect(() => {
-    let map;
-    let draw;
-
-    if (!mapRef.current) {
-      map = new Map({
-        target: "map",
-        layers: [
-          new TileLayer({
-            source: new OSM(),
-          }),
-          vectorLayer.current,
-        ],
-        view: new View({
-          center: [0, 0],
-          zoom: 2,
-        }),
-      });
-
-      draw = new Draw({
-        source: vectorSource.current,
-        type: drawType,
-        style: new Style({
-          fill: new Fill({
-            color: "rgba(173, 216, 230, 0.6)",
-          }),
-          stroke: new Stroke({
-            color: "#ffcc33",
-            width: 2,
-          }),
-          image: new CircleStyle({
-            radius: 7,
-            fill: new Fill({
-              color: "#ffcc33",
-            }),
-          }),
-        }),
-        active: drawActive,
-      });
-
-      // Add event listener for drawStart
-      draw.on("drawstart", (event) => {
-        setDrawFeature(event.feature);
-      });
-
-      // Add event listener for drawEnd
-      draw.on("drawend", (event) => {
-        // Force LineString to close at starting coordinate
-        const geometry = event.feature.getGeometry();
-        if (geometry instanceof LineString) {
-          const coordinates = geometry.getCoordinates();
-          coordinates.push(coordinates[0]); // Add the starting coordinate to close the LineString
-          geometry.setCoordinates(coordinates);
-
-          // Create a polygon feature with the same coordinates as the LineString
-          const polygon = new Polygon([coordinates]);
-          const polygonFeature = new Feature({
-            geometry: polygon,
-          });
-
-          // Add the polygon feature to the vector source
-          vectorSource.current.addFeature(polygonFeature);
-        }
-      });
-
-      setDrawFeature(null);
-
-      map.addInteraction(draw);
+    // Checks if the map is already there - debugged multiple maps being created
+    if (map !== null) {
+      return;
     }
 
+    // Creates an icon feature for the map, no idea if this is actually working
+    const iconFeature = new Feature({
+      geometry: new Point([0, 0]),
+      name: "Null Island",
+      population: 4000,
+      rainfall: 500,
+    });
+
+    // Create a style for the icon
+    const style = new Style({
+      text: new Text({
+        anchor: [0.5, 46],
+        anchorXUnits: "fraction",
+        anchorYUnits: "pixels",
+        text: textValue, // Use the state value
+      }),
+    });
+
+    const raster = new TileLayer({
+      source: new OSM(),
+    });
+
+    const source = new VectorSource({ wrapX: false, features: [iconFeature] });
+
+    const vectorLayer = new VectorLayer({
+      source: source,
+      style: function () {
+        // apply a style config object to each feature
+        style.getText().setText(textValue);
+        return style;
+      },
+    });
+
+    const newMap = new Map({
+      layers: [raster, vectorLayer],
+      // uses the target ref to create the map
+      target: mapContainerRef.current,
+      view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4,
+      }),
+    });
+
+    const modify = new Modify({
+      hitDetection: vectorLayer,
+      source: source,
+    });
+
+    // Handle cursor styles during modification interactions
+    modify.on(["modifystart", "modifyend"], function (evt) {
+      mapContainerRef.current.style.cursor =
+        evt.type === "modifystart" ? "grabbing" : "pointer";
+    });
+
+    const overlaySource = modify.getOverlay().getSource();
+
+    // Handle cursor styles during overlay source events
+    overlaySource.on(["addfeature", "removefeature"], function (evt) {
+      mapContainerRef.current.style.cursor =
+        evt.type === "addfeature" ? "pointer" : "";
+    });
+
+    newMap.addInteraction(modify);
+
+    setMap(newMap);
+
+    // Remove the draw interaction when the component unmounts
     return () => {
-      if (map) {
-        map.setTarget(null);
+      if (drawInteraction) {
+        newMap.removeInteraction(drawInteraction);
       }
     };
-  }, [drawActive, drawType]);
+  }, [textValue]);
 
-  const toggleDraw = () => {
-    setDrawActive((prevDrawActive) => !prevDrawActive);
-  };
+  // Creates or updates the draw based on the selected geometry type
+  useEffect(() => {
+    if (map && type !== "None") {
+      if (drawInteraction) {
+        map.removeInteraction(drawInteraction);
+      }
 
-  const toggleDrawType = () => {
-    setDrawType((prevDrawType) =>
-      prevDrawType === "Point" ? "LineString" : "Point"
-    );
-  };
+      const newDrawInteraction = new Draw({
+        source: map.getLayers().item(1).getSource(), // Assuming vector layer is at index 1
+        type: type,
+        freehand: true,
+      });
+
+      newDrawInteraction.on("drawend", (event) => {
+        const geometry = event.feature.getGeometry();
+        addUserFeature(geometry);
+        // Test geometry call
+        console.log("User added feature: ", geometry);
+      });
+
+      map.addInteraction(newDrawInteraction);
+      setDrawInteraction(newDrawInteraction);
+    }
+  }, [map, type]);
+
+  // Toggle the icon feature visibility
+  useEffect(() => {
+    if (map) {
+      const vectorLayer = map.getLayers().item(1); // Assuming vector layer is at index 1
+      if (vectorLayer) {
+        vectorLayer.setVisible(true); // Always show icon feature
+      }
+    }
+  }, [map]);
 
   return (
     <div>
-      <div id='map' style={{ width: "100%", height: "400px" }}></div>
-      <button onClick={toggleDraw}>
-        {drawActive ? "Disable Draw" : "Enable Draw"}
-      </button>
-      <button onClick={toggleDrawType}>
-        {drawType === "Point" ? "Switch to LineString" : "Switch to Point"}
-      </button>
-      {drawFeature && (
-        <div>
-          <h4>Drawn Feature Info:</h4>
-          <p>Type: {drawFeature.getGeometry().getType()}</p>
-          <p>
-            Coordinates:{" "}
-            {JSON.stringify(drawFeature.getGeometry().getCoordinates())}
-          </p>
-        </div>
-      )}
+      <div ref={mapContainerRef} className='map'></div>
+      <form>
+        <label htmlFor='type'>Geometry type &nbsp;</label>
+        <select id='type' value={type} onChange={handleTypeChange}>
+          <option value='LineString'>LineString</option>
+          <option value='Polygon'>Polygon</option>
+          <option value='Circle'>Circle</option>
+          <option value='Point'>Point</option>
+        </select>
+      </form>
+      <div>
+        <h2>Points of Interest:</h2>
+        <ul>
+          {userFeatures.map((feature, index) => (
+            <li key={index}>
+              Feature {index + 1}: {textValue}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* Display the text input */}
+      <div>
+        <label htmlFor='textValue'>Text Value: &nbsp;</label>
+        <input
+          type='text'
+          id='textValue'
+          value={textValue}
+          onChange={handleTextChange}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 };
