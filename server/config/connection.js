85fe8364c0c95d29d18c3655352930f7b2c27cd0
Brandon Barnes <imbrandonbarnes@gmail.com>
2024-01-08 10:29:51 -0800

    Installed Postgres Changed connection from MongoDB to Postgres
    ENV Update :
    -------------------
    PG_USER=
    PG_HOST=
    PG_DATABASE=
    PG_PASSWORD=
    PG_PORT=
    -------------------

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,28 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+//
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/PARSR");
+//
+// module.exports = mongoose.connection;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/PARSR");
+const { Pool } = require('pg');
+const path = require('path');
+require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
 
-module.exports = mongoose.connection;
+console.log(process.env.PG_USER);
+
+const pool = new Pool({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+
+pool.query('SELECT NOW()', (err, result) => {
+    if (err) {
+        return console.error('Error executing query', err);
+    }
+    console.log(`Successfully connected to database at ${result.rows[0].now} on port ${process.env.PG_PORT}`);
+    pool.end();
+});
